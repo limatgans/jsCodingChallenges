ba9ad28b543fffafe1534b227a66b6684a3fd0dd
limatgans <limatgans@gmail.com>
2019-03-01 12:30:18 +0530

    [New File] 17_isPropTrueEverywhere.js

---
+++ Intermediate/17_isPropTrueEverywhere.js
@@ -0,0 +1,34 @@
+/*
+Problem Statement
+
+You are given an array collection of objects. 
+The predicate pre will be an object property and you need to return true if its value is truthy. 
+Otherwise, return false.
+
+In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+Expected Outputs:
+
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
+truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.
+truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age") should return false.
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastFoward", "onBoat": null}], "onBoat") should return false
+truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastFoward", "onBoat": true}], "onBoat") should return true
+truthCheck([{"single": "yes"}], "single") should return true
+truthCheck([{"single": ""}, {"single": "double"}], "single") should return false
+truthCheck([{"single": "double"}, {"single": undefined}], "single") should return false
+truthCheck([{"single": "double"}, {"single": NaN}], "single") should return false
+*/
+
+function truthCheck(collection, pre) {
+    // Is everyone being true?
+    return collection.every((item) => {
+        if (!item.hasOwnProperty(pre)) {
+            return false;
+        }
+        return Boolean(item[pre]);
+    });
+}
+
+truthCheck([{ "user": "Tinky-Winky", "sex": "male" }, { "user": "Dipsy", "sex": "male" }, { "user": "Laa-Laa", "sex": "female" }, { "user": "Po", "sex": "female" }], "sex");
+
